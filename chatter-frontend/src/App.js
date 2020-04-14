f06672bf514281653353b98c24cd664ec4c2a7a2
sravanthidasari <sravanthidasari@hotmail.com>
2020-04-13 19:09:33 -0700

    Adding querying news with a date

--- chatter-frontend/src/App.js
+++ chatter-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ArticleCard from "./components/ArticleCard";
 import { getNewsArticlesForToday, getNewsArticleDetails, addCommentToArticle, addArticleReaction } from "./data";
-import moment from 'moment';
+import moment from "moment";
 
 import "./assets/main.css";
 import "./App.css";
@@ -21,10 +21,39 @@ class App extends React.Component {
     this.selectArticle = this.selectArticle.bind(this);
     this.addComment = this.addComment.bind(this);
     this.postArticleReaction = this.postArticleReaction.bind(this);
+    this.goPrevious = this.goPrevious.bind(this);
+    this.goNext = this.goNext.bind(this);
+    this.startOver = this.startOver.bind(this);
+    this.backToList = this.backToList.bind(this);
   }
 
   componentDidMount() {
     this.getData();
+  }
+
+  async startOver() {
+    this.setState({
+      selectedArticle: undefined,
+      showArticleMain: false,
+      date: moment().startOf("date")
+    });
+  }
+
+  async backToList() {
+    this.setState({
+      selectedArticle: undefined,
+      showArticleMain: false
+    });
+  }
+
+  async goPrevious(event) {
+    event.preventDefault();
+    this.setState({ date: this.state.date.subtract(1, "days") });
+  }
+
+  async goNext(event) {
+    event.preventDefault();
+    this.setState({ date: this.state.date.add(1, "days") });
   }
 
   async getData() {
@@ -92,33 +121,79 @@ class App extends React.Component {
     });
   }
 
+  renderNextButton() {
+    let isDisabled = this.state.date.isSameOrAfter(moment(), "day");
+
+    return isDisabled ? (
+      <button
+        className="bg-gray-300 font-bold py-2 px-4 rounded-r ml-5 opacity-50 text-gray-500 cursor-not-allowed"
+        disabled
+      >
+        Next
+      </button>
+    ) : (
+      <button
+        className="bg-gray-300 font-bold py-2 px-4 rounded-r ml-5 hover:bg-gray-400 text-gray-800"
+        onClick={this.goNext}
+      >
+        Next
+      </button>
+    );
+  }
+
+  renderPrevButton() {
+    let isDisabled = this.state.date.isSameOrBefore(moment().startOf("date").subtract(7, "days"), "day");
+
+    return isDisabled ? (
+      <button
+        className="bg-gray-300 font-bold py-2 px-4 rounded-l mr-5 opacity-50 text-gray-500 cursor-not-allowed"
+        disabled
+      >
+        Prev
+      </button>
+    ) : (
+      <button
+        className="bg-gray-300 font-bold py-2 px-4 rounded-l mr-5 hover:bg-gray-400 text-gray-800"
+        onClick={this.goPrevious}
+      >
+        Prev
+      </button>
+    );
+  }
+
   renderNav() {
     return (
       <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
-        <div className="flex items-center flex-shrink-0 text-white w-32">
-          <span className="font-bold text-xl">Chatter.news</span>
+        <div className="flex items-center flex-shrink-0 text-white w-32 cursor-pointer" onClick={this.startOver}>
+          <span className="font-bold text-3xl">Chatter.news</span>
         </div>
         <div className="inline-flex">
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l mr-5">Prev</button>
+          {!this.state.showArticleMain ? this.renderPrevButton() : null}
           <div className="font-bold text-xl text-white">{this.state.date.format("MMM Do")}</div>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r ml-5">Next</button>
+          {!this.state.showArticleMain ? this.renderNextButton() : null}
         </div>
         <div className="flex items-center flex-shrink-0 text-white w-32 font-bold">
-          <p class="text-right w-full">Login</p>
+          <p className="text-right w-full">Login</p>
         </div>
       </nav>
     );
   }
 
   renderArticleMain() {
-    return <ArticleMain article={this.state.selectedArticle} onAddComment={this.addComment} />;
+    return <ArticleMain article={this.state.selectedArticle} onAddComment={this.addComment} onBackToList={this.backToList} />;
   }
 
   renderArticleList() {
     return (
       <div className="flex flex-wrap justify-center">
         {this.state.articles.map((a, index) => (
-          <ArticleCard className="w-full md:w-5/12" article={a} key={index} onArticleSelected={this.selectArticle} onPostReaction={this.postArticleReaction} />
+          <ArticleCard
+            className="w-full md:w-5/12"
+            article={a}
+            key={index}
+            onArticleSelected={this.selectArticle}
+            onPostReaction={this.postArticleReaction}
+          />
         ))}
       </div>
     );
